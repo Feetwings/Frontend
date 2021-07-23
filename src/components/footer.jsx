84011e09ff4e5806cba0615cf2b3e97f6f1daa36
Chetna Gola <chetnagola@gmail.com>
2021-07-23 21:11:20 +0530

    issue9

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ function footer(){
         <footer class="footer-area pt-200">
                 <div class="container">
                 </div>
-                <div class="top button" align="right">
+                <div class="topButton" align="right">
                 <button id="Mybtn"><a href="#home"> Back to Top</a></button>
                 </div>
                 <div class="copyright-text">

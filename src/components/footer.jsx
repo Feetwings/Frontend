d4d952a15bfbbc5306a61c5a4218440aec6fe2a6
Chetna Gola <chetnagola@gmail.com>
2021-07-23 19:25:23 +0530

    issue9

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function footer(){
     return (
         <footer class="footer-area pt-200">
-            <div class="container">
+                <div class="container">
                 </div>
-
+                <div class="top button" align="right">
+                <button id="Mybtn"><a href="#home"> Back to Top</a></button>
+                </div>
                 <div class="copyright-text">
                     <p>Copyright @2021 FeetWings. All Rights Reserved </p>
                 </div>

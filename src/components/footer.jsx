8042af6203c2897cb578f0963f0c0e97bb693971
Chetna Gola <chetnagola@gmail.com>
2021-07-26 11:18:05 +0530

    Issue9

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,8 +5,8 @@ function footer(){
         <footer class="footer-area pt-200">
                 <div class="container">
                 </div>
-                <div class="topButton" align="right">
-                <button id="Mybtn"><a href="#home"> Back to Top</a></button>
+                <div class="backToTop" align="right">
+                <button id="myButton"><a href="#home"> Back to Top</a></button>
                 </div>
                 <div class="copyright-text">
                     <p>Copyright @2021 FeetWings. All Rights Reserved </p>

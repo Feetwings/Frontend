41f37ad1c4b86d28d3de89f35ca43af341215fa1
Arya Soni <aryasoni98@gmail.com>
2022-04-03 01:52:06 +0530

    Deploy

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-    const element = render( < App / > );
-    const linkElement = element.getByText(/FeetWings Web/i);
-    expect(linkElement).toBeInTheDocument();
-  });

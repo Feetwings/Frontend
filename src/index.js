acb92195bf2fbaf8d1730765df4ebbb41e63b6e9
Arya Soni <aryasoni98@gmail.com>
2022-04-02 22:42:15 +0530

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import reportWebVitals from './reportWebVitals';
 
 import ReactDOM from "react-dom";
 import App from "./App.js";
@@ -7,3 +8,4 @@ import App from "./App.js";
 ReactDOM.render( <App />,  document.getElementById("root"));
 
 
+reportWebVitals();

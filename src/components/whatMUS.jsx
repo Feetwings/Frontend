697f932cf40a41e0b709c3578666e8fe093b5b40
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-04-05 00:59:35 +0530

    smart-socks-section-light-theme

--- src/components/whatMUS.jsx
+++ src/components/whatMUS.jsx
@@ -1,46 +1,65 @@
 import React from "react";
 
 function WMUS() {
-  return (
-    <div id="smart" class="app-banner-area pt-100">
-        <div class="container">
-            <div class=" banner-box">
-                <div class="col-lg-6 img-box text-center">
-                <div className="banner-title-container1">
-                <h1 class="banner-title">Smart Socks </h1>
-                <h6 class="banner-owner">powered by</h6>
-                <h2 class="banner-title">Smart Yarn</h2>
-                </div>
-                    <div class="app-banner-img">
-                    <img src="../assets/landing_img/fibre.svg" class="app-img" alt="FeetWings" />
-                    <div class="app-shapes"></div>
-                    </div>
-                </div>
-                <div class="content-box">
-                    <div class=" ">
-                        <div className="banner-title-container2">
-                        <h1 class="banner-title">Smart Socks </h1>
-                        <h6 class="banner-owner">powered by</h6>
-                        <h2 class="banner-title">Smart Yarn</h2>
+    return (
+        <div id="smart" class="app-banner-area pt-100">
+            <div class="container2">
+                <div class=" ">
+                    <div class="banner-box">
+                        <div class="left-banner">
+                            <div class=" ">
+                                <div className="banner-title-new">
+                                    <h1 class="banner-title-text-new">Smart socks </h1>
+                                    <h6 class="banner-owner-text-new">powered by</h6>
+                                    <h2 class="banner-title-text-new">Smart Yarn</h2>
+                                </div>
+                            </div>
+                            <div class="app-bennr-text-new visible">
+                                <p>
+                                    Introducing innovative smart textile technology that allows us
+                                    to embed state of the art sensors as electronic strands right at
+                                    the core of the yarn. The strand is extremely flexible and gives
+                                    your feet enough breathing room and can be washed and dried just
+                                    like your regular dumb socks!
+                                </p>
+                            </div>
+                            <div class="btn-container-new visible">
+                                <button class="btn-new">Learn More</button>
+                            </div>
+                        </div>
+
+                        <div class="img-box-new">
+                            <img src="../assets/landing_img/fibre2.0.svg" class="" alt="FeetWings" />
+                            <div class="app-shapes"></div>
+                        </div>
+                        <div class="left-banner hidden">
+                            <div class=" ">
+                                <div className="banner-title-new visible">
+                                    <h1 class="banner-title-text-new">Smart socks </h1>
+                                    <h6 class="banner-owner-text-new">powered by</h6>
+                                    <h2 class="banner-title-text-new">Smart Yarn</h2>
+                                </div>
+                            </div>
+                            <div class="app-bennr-text-new">
+                                <p>
+                                    Introducing innovative smart textile technology that allows us
+                                    to embed state of the art sensors as electronic strands right at
+                                    the core of the yarn. The strand is extremely flexible and gives
+                                    your feet enough breathing room and can be washed and dried just
+                                    like your regular dumb socks!
+                                </p>
+                            </div>
+                            <div class="btn-container-new">
+                                <button class="btn-new">Learn More</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="app-bennr-text">
-                        <p>
-                            Introducing innovative smart textile technology that allows us
-                            to embed state of the art sensors as electronic strands right at
-                            the core of the yarn. The strand is extremely flexible and gives
-                            your feet enough breathing room and can be washed and dried just
-                            like your regular dumb socks!
-                        </p>
-                    </div>
+
                 </div>
-            </div>
-            <div class="btn_container">
-                <button class="btn">More</button>
+
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default WMUS;
